674be56602de8c862c96beaf57d2e4b4c5fdfd41
unknown <you@example.com>
2022-12-31 16:18:49 +0200

    add frontend template

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,11 @@ app_name ='blog'
 urlpatterns = [
     path('',views.post_list,name='post_list'),
     path('<int:id>/',views.post_detail,name='post_detail'),
+    path('new/',views.new_post,name='new_post'),
+    path('<int:id>/edit',views.edit_post,name='edit_post'),
+    path('<int:id>/delete',views.delete_post,name='delete_post'),
+    # path('cbv',views.PostList.as_view(),name='cbv_post_list'),
+    # path('cbv/<int:pk>',views.PostDetail.as_view(),name='cbv_post_detail'),
+    # path('cbv/<int:pk>/delete',views.PostDelete.as_view(),name='cbv_post_delete'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from.models import Post
+from . forms import PostForm
 # Create your views here.
 
 def post_list(request):
@@ -10,3 +11,41 @@ def post_list(request):
 def post_detail(request,id):
     post = get_object_or_404 (Post,id=id)
     return render(request,'blog/post_detail.html',{'post':post})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PostForm()
+    return render (request,'blog/new_post.html',{'form':form})
+
+
+def edit_post(request,id):
+    post = get_object_or_404 (Post,id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PostForm(instance=post)
+    return render (request,'blog/new_post.html',{'form':form})
+
+def delete_post(request,id):
+    post = get_object_or_404 (Post,id=id)
+    post.delete()
+    return redirect('/blog')
+
+
+# from django.views.generic import ListView,DetailView,DeleteView
+
+# class PostList(ListView):
+#     model = Post
+
+# class PostDetail(DetailView):
+#     model = Post
+
+# class PostDelete(DeleteView):
+#     model = Post
+#     success_url = '/blog/cbv'
